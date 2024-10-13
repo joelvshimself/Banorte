2ba541827fec4808390dd02d85d6f2945f9509dd
Joel Vargas Reynoso <76189923+joelvshimself@users.noreply.github.com>
2024-10-12 18:17:56 -0600

    Initial commit

---
+++ Banorte/BanorteApp.swift
@@ -0,0 +1,17 @@
+//
+//  BanorteApp.swift
+//  Banorte
+//
+//  Created by Joel Vargas on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BanorteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BanorteTests/BanorteTests.swift
@@ -0,0 +1,16 @@
+//
+//  BanorteTests.swift
+//  BanorteTests
+//
+//  Created by Joel Vargas on 12/10/24.
+//
+
+import Testing
+
+struct BanorteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
